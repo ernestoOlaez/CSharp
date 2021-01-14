1e25701c687ed1a67bc6a0aea4364e391473615e
Ernesto Olaez <olaez_ernesto@hotmail.com>
2021-01-14 08:15:07 -0600

    New Code SolveMeFirst

---
+++ CSharp/CodesFromWeb/SolveMeFirst/SolveMeFirst.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.CodesFromWeb.SolveMeFirst
+{
+    class SolveMeFirst
+    {
+        //svm
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solve me First:");
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            
+            int sum = 0;
+           sum= functionSolveMeFirst(val1, val2);
+            Console.WriteLine("The sum =" + sum);
+
+
+        }
+
+        static int  functionSolveMeFirst(int val1, int val2)
+        {
+           
+            return val1 + val2;
+        }
+
+    }
+}
