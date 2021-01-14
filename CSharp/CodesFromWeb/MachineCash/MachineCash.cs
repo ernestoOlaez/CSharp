6572ecb2cf4057428e226a037af05015f2436306
ernestoOlaez <olaez_ernesto@hotmail.com>
2021-01-14 15:59:47 -0600

    Add  Class

---
+++ CSharp/CodesFromWeb/MachineCash/MachineCash.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.CodesFromWeb.MachineCash
+{
+    class MachineCash
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inser the amount to change:");
+            int amount = Convert.ToInt32(Console.ReadLine());
+
+            int count = 0;
+            List<int> numbers = new List<int>() { 1, 2, 5, 10, 20 };
+            numbers.Reverse();
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+                if (amount % i != 0)
+                {
+                    amount -= i;
+                    count++;
+                }
+
+
+            }
+            Console.WriteLine("The number of coins are: " + count);
+            Console.ReadLine();
+        }
+    }
+}
