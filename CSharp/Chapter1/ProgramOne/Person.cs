9e3146558d4da9aa68efd7c0cf362a0fd905a633
ernestoOlaez <olaez_ernesto@hotmail.com>
2021-01-14 17:11:07 -0600

    Add new class

---
+++ CSharp/Chapter1/ProgramOne/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Chapter1.ProgramOne
+{
+    class Person
+    {
+        // Name of person
+        public String Name;
+
+        // Year of Birth
+        public int BornYear;
+
+        // Place of Birth
+        public String PlaceOfBirth;
+
+        // This Constructor has 3 parameters. 
+        // The aim to initialize the value for fields of Person.
+        // Specify the name, year of birth, place of birth.
+        public Person(String Name, int BornYear, String PlaceOfBirth)
+        {
+            this.Name = Name;
+            this.BornYear = BornYear;
+            this.PlaceOfBirth = PlaceOfBirth;
+        }
+
+        // This Constructor have 2 parameters. 
+        // Purpose initialize for two fields (name & Born year) of Person.
+        // PlaceOfBirth not be initialized.
+        public Person(String Name, int BornYear)
+        {
+            this.Name = Name;
+            this.BornYear = BornYear;
+        }
+
+    }
+}
---
+++ CSharp/Chapter1/ProgramOne/PersonDemo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CSharp.Chapter1.ProgramOne
+{
+    class PersonDemo
+    {
+
+        static void Main(string[] args)
+        {
+
+            // Instance: Thomas Edison.
+            // Created by Constructor has 2 parameters of the Person class.
+            Person edison = new Person("Thomas Edison", 1847);
+
+            Console.WriteLine("Info:");
+            Console.WriteLine("Name: " + edison.Name);
+            Console.WriteLine("Born Year: " + edison.BornYear);
+            Console.WriteLine("Place Of Birth: " + edison.PlaceOfBirth);
+
+            // Instance: Bill Gates.
+            // Created by Constructor has 3 parameters of the Person class.
+            Person billGates = new Person("Bill Gate", 1955, "Seattle, Washington");
+
+            Console.WriteLine("-----------------------------------");
+
+            Console.WriteLine("Info:");
+            Console.WriteLine("Name: " + billGates.Name);
+            Console.WriteLine("Born Year: " + billGates.BornYear);
+            Console.WriteLine("Place Of Birth: " + billGates.PlaceOfBirth);
+
+            Console.ReadLine();
+        }
+    }
+}
+
